5d2a65ddda5c3045206ce22628dff339d8039701
Navina K <99734360+navina1@users.noreply.github.com>
2023-09-29 20:51:58 +0530

    longest palindromic sequence

---
+++ Hashing/longestPalindromicSubstring.js
@@ -0,0 +1,24 @@
+function expandfromCenter(str, left, right) {
+    let i = 0;
+    while (str[left - i] && str[left - i] == str[right + i]) {
+        i++;
+    }
+    i--;
+    return str.slice(left - i, right + i + 1);
+}
+function longestPalindromicSubstring(s) {
+    // TODO: Implement your code here
+    let longest = "";
+    for (let i = 0; i < s.length - 1; ++i) { 
+        let oddPalindrome = expandfromCenter(s,i,i);
+        let evenPalindrome = expandfromCenter(s, i - 1, i);
+        if (oddPalindrome.length > longest.length) {
+            longest = oddPalindrome;
+        }
+        if (evenPalindrome.length > longest.length) {
+            longest = evenPalindrome;
+        }
+    }
+    return longest;
+}
+console.log(longestPalindromicSubstring("abbaaa"));
